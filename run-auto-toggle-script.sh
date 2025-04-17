b26650847664524e02493d99b0955abde429d8a0
LightWayUp <lightwayuplightwayup@gmail.com>
2025-04-17 15:50:57 +0800

    Rename Python source files
    Rename Python source files to allow imports

--- run-auto-toggle-script.sh
+++ run-auto-toggle-script.sh
@@ -4,4 +4,4 @@ SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
 cd $SCRIPT_DIR
-python3 ./src/auto-toggle-touch-mode.py
+python3 ./src/auto_toggle_touch_mode.py
--- run-toggle-script.sh
+++ run-toggle-script.sh
@@ -4,4 +4,4 @@ SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
 cd $SCRIPT_DIR
-python3 ./src/toggle-touch-mode.py
+python3 ./src/toggle_touch_mode.py

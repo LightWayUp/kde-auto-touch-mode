cf18b229792435b6ccce8bd4b596f8b318eadeab
LightWayUp <lightwayuplightwayup@gmail.com>
2025-04-18 03:59:12 +0800

    Fix `TabletMode` auto configuration
    - Remedy MRDGH2821/kde-auto-touch-mode#1 . Events
    of the same device action in a events batch
    (defined as <= 0.1 second between such events,
    from empirical testing) only applies configuration
    once.
    - Filter out device events where the action isn't
    `"added"` or `"removed"`
    - Fix setting `TabletMode` configuration value.
    The code of `toggle_mode()` function did not match
    its description; device action was not considered
    at all, instead `TabletMode` was always toggled.
    If the configuration was modified externally (for
    example via System Settings), or if the function
    was invoked even number of times per device
    addition or removal, the configuration state the
    script assumed would be out of sync with the
    actual state.
    - Stop catching `KeyboardInterrupt`. This was done
    to stop `MonitorObserver` background thread, which
    is not used here anymore. Allow `KeyboardInterrupt`
    to end the process.

--- src/auto_toggle_touch_mode.py
+++ src/auto_toggle_touch_mode.py
@@ -4,8 +4,9 @@
 Automatically Toggles touch mode when a touchpad is connected or disconnected.
 """
 
-import toggle_touch_mode
+from toggle_touch_mode import TabletMode
 
+import time
 import logging
 from pathlib import Path
 
@@ -19,31 +20,48 @@ current_file_directory = Path(__file__).resolve().parent
 # TODO(MRDGH2821): reject further calls done in quick succession. Check `logs.log` for more info.
 # https://github.com/MRDGH2821/kde-auto-touch-mode/issues/1
 # Duplicate calls are happening due to same device being added 3 times with different properties.
-def toggle_mode(device: Device) -> None:
-    """Toggle touch mode based on the device action."""
-    if device.properties.get("ID_INPUT_TOUCHPAD") != "1":
-        return
+#def toggle_mode(device: Device) -> None:
+#    """Toggle touch mode based on the device action."""
+#    if device.properties.get("ID_INPUT_TOUCHPAD") != "1":
+#        return
+#
+#    if device.action == "add":
+#        logging.info("Touchpad detected. Deactivating touch mode...")
+#    else:
+#        logging.info("Touchpad disconnected. Activating touch mode...")
+#    TabletMode.apply_toggled()
 
-    if device.action == "add":
-        logging.info("Touchpad detected. Deactivating touch mode...")
-    else:
-        logging.info("Touchpad disconnected. Activating touch mode...")
-    toggle_touch_mode.toggle()
+def monitor() -> None:
+    # Set up udev monitor
+    context = Context()
+    monitor = Monitor.from_netlink(context)
+    monitor.filter_by("input")
 
-
-logging.debug(
-    "Auto toggler started",
-    extra={
-        "cwd": current_file_directory,
-    },
-)
-
-# Set up udev monitor
-context = Context()
-monitor = Monitor.from_netlink(context)
-monitor.filter_by("input")
-try:
+    last_mode = ""
+    last_time = 0
     for device in iter(monitor.poll, None):
-        toggle_mode(device)
-except KeyboardInterrupt:
-    pass
+        if device.properties.get("ID_INPUT_TOUCHPAD") != "1":
+            continue
+        action = device.action
+        if not action in ("add", "remove"):
+            continue
+
+        now = time.time_ns()
+        should_configure = False
+        if last_mode == action:
+            should_configure = now - last_time > 100000000 # 0.1 second
+        else:
+            last_mode = action
+            should_configure = True
+        last_time = now
+        if should_configure:
+            TabletMode.apply(TabletMode.OFF if action == "add" else TabletMode.ON)
+
+if __name__ == "__main__":
+    logging.debug(
+        "Auto toggler started",
+        extra={
+            "cwd": current_file_directory,
+        },
+    )
+    monitor()
